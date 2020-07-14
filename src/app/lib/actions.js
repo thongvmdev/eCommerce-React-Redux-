46e70b887a91ef30db000ab9117f6ec15ade3181
Minh Thong <vominhthong1603@gmail.com>
2020-07-14 11:28:01 +0700

    20200714

---
+++ src/app/lib/actions.js
@@ -0,0 +1,63 @@
+import { v4 as uuidv4 } from 'uuid';
+
+/* Action types */
+export const actions = {
+    ADD_TO_CART: "ADD_TO_CART",
+    UPDATE_CART: "UPDATE_CART",
+    REMOVE_FROM_CART: "REMOVE_FROM_CART"
+}
+
+/* Action Creators */ 
+export function addToCart(item, quantity) {
+    return {
+        type: actions.ADD_TO_CART,
+        payload: { id: uuidv4(), quantity: quantity, details: item}
+    }
+}
+
+export function updateCart(item, quantity) {
+    return {
+        type: actions.UPDATE_CART,
+        payload: {item: item, quantity: quantity}
+    }
+}
+
+export function removeFromCart(item) {
+    return {
+        type: actions.REMOVE_FROM_CART,
+        payload: item
+    }
+}
+
+// export const actions = {
+//     GET_ITEMS: "GET_ITEMS",
+//     ADD_TO_CART: "ADD_TO_CART",
+//     UPDATE_CART: "UPDATE_CART",
+//     REMOVE_FROM_CART: "REMOVE_FROM_CART",
+//     SAVE_CART: "SAVE_CART"
+//   };
+// const uid = () => Math.random().toString(34).slice(2);
+// export const addToCart = (item, quantity) => {
+// return {
+//  type: actions.ADD_TO_CART,
+//  payload: { id: uid(), quantity: quantity, details: item }
+// };
+// }
+// export const updateCart = (item, quantity) => {
+// return{ 
+// type: actions.UPDATE_CART,
+// payload: {item: item, quantity: quantity}
+// }
+// }
+// export const removeFromCart = item => {
+// return {
+// type: actions.REMOVE_FROM_CART,
+// payload: item
+// };
+// };
+// export const saveCart = (items) => {
+// return{ 
+// type: actions.SAVE_CART,
+// payload: {items: items}
+// }
+// }
---
+++ src/app/lib/reducers.js
@@ -0,0 +1,63 @@
+import { actions } from './actions'
+
+const initiaState = {
+    items: []
+}
+
+export default function onlineStoreApp(state = initiaState, action) {
+    switch (action.type) {
+        case actions.ADD_TO_CART: return Object.assign({}, state, {items: [...state.items, action.payload]});
+        case actions.UPDATE_CART: return Object.assign({}, state, {
+            items: state.items.map(item => {
+                return item.id === action.payload.item.id 
+                ? Object.assign({}, item, { quantity: action.payload.quantity })
+                : item;
+            })
+        });
+        case actions.REMOVE_FROM_CART: return Object.assign({}, state, {
+            items: state.items.filter(item => { return item.id != action.payload.id })
+        })
+    }
+}
+
+
+// import { actions } from './actions'
+// const { ADD_TO_CART, SAVE_CART, UPDATE_CART, REMOVE_FROM_CART } = actions
+ 
+// const saveToLocalstorage = object => {
+//    localStorage.setItem("items", JSON.stringify(object));
+// }
+// const initialState = {
+//   items:
+//     JSON.parse(localStorage.getItem("items")) != null
+//       ? JSON.parse(localStorage.getItem("items"))
+//       : [], 
+//   total: 0
+// };
+// export default function onlineStoreApp(state = initialState, action) {
+//     switch (action.type) {
+//       case ADD_TO_CART: 
+//       return Object.assign({}, state, {items: [...state.items, action.payload]});
+//       case UPDATE_CART:
+//         return Object.assign({}, state, {
+//           items: state.items.map(item => {
+//             return item.id === action.payload.item.id
+//               ? Object.assign({}, item, {
+//                   quantity: action.payload.quantity
+//                 })
+//               : item;
+//           })
+//         });
+//       case REMOVE_FROM_CART:
+//         return Object.assign({}, state, {
+//           items: state.items.filter(item => {
+//             return item.id != action.payload.id;
+//           })
+//         });
+//       case SAVE_CART:
+//         saveToLocalstorage(action.payload.items);
+//         return Object.assign({}, state, {items: action.payload.items});
+//       default:
+//         return state;
+//     }
+// }   
