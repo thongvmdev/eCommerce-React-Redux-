e2dc91fd784b42d29f0446b5cb77dfd4ec1d9db0
thongvm316 <vominhthong1603@gmail.com>
2020-07-21 09:55:35 +0700

    Next

--- src/app/views/components/Home.js
+++ src/app/views/components/Home.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 import { List } from '../../components'
 
@@ -25,31 +24,3 @@ export const Home = (props) => {
         </div>
     )
 }
-=======
-import React from 'react'
-import { List } from '../../components'
-
-const SideMenu = ({ localCategory, category }) => {
-    const links = ["Légumes", "Fruits", "Produits Frais", "Epicerie", "Boissons"]
-    return (
-      <div className="col-sm-2 sidebar">
-        <ul>
-          {links.map((link, index) => <li className={category === index ? 'active' : undefined} onClick={() => localCategory(index)} key={index}>{link}</li>)}
-        </ul>
-      </div>
-    )
-}
-
-export const Home = (props) => {
-    const { category, localCategory, addToCart, updateToCart, list, isFiltering, filtered  } = props;
-    return(
-        <div className="container">
-            <div className="row">
-              <SideMenu localCategory={localCategory} category={category}/>
-              {/* <List data={ isFiltering ? filtered : list[category]} count={count} addToCart={addToCart}/> */}
-              <List data={ isFiltering ? filtered : list[category]} updateToCart={updateToCart}  addToCart={addToCart}/>
-            </div>
-        </div>
-    )
-}
->>>>>>> 5eeeb38d16e321d0ef0f0e9446ccaef68382ca0c
